228d6f1a6081e85cbd3f8462d057fee638b31d5d
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2016-04-19 12:24:19 -0400

    basic form complete

---
+++ src/js/main.js
@@ -0,0 +1,66 @@
+import $ from 'jquery';
+//import 'font-awesome';
+
+var url = "http://json-data.herokuapp.com/forms"
+var cont = $(".container");
+
+var dataReq = $.getJSON(url);
+dataReq.then(function(res){
+  console.log(res);
+  buildForm(res);
+});
+
+function selectopt(resobj){
+  return`
+     <div class="formElement" id="${resobj.id}">
+     <select> 
+      <option>Select your Language</option>
+
+     </select> 
+    </div>
+  `;
+  
+  
+};
+
+function textarea(resobj){
+  return`
+     <div class="formElement" id="${resobj.id}">
+      <textarea placeHolder= "${resobj.label}"></textarea>
+      <i class="fa ${resobj.icon}"></i>
+      
+    </div>
+    `;
+
+};
+
+ function input(resobj){
+   return `
+    <div class="formElement" id="${resobj.id}">
+      <input type="${resobj.type}" placeHolder="${resobj.label}">
+      <i class="fa ${resobj.icon}"></i>
+</div>
+`;
+ };
+var pageData;
+
+function buildForm(dataArr){
+  dataArr.forEach(function(element){
+    console.log("element", element);
+    if(element.type ==="textarea"){
+      pageData = textarea(element);
+      cont.append(pageData);
+    }else if(element.type ==="text" || element.type ==="email" || element.type ==="tel" ){
+      pageData = input(element);
+      cont.append(pageData);
+    }
+    else{
+      pageData = selectopt(element);
+      cont.append(pageData);
+    }
+
+    //pageData = input(element);
+    
+  })
+}
+
